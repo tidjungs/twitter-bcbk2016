6b473461a7ebfd9dfbf6d47b59c9b439f3ac2fa5
tidjungs <waewprach.s@ku.th>
2016-09-10 13:30:12 +0700

    add scss webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -107,6 +107,10 @@ module.exports = {
         test: /\.css$/,
         loader: 'style!css!postcss'
       },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass!postcss'
+      },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
       {
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './test.scss';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <h1 className="greeting">Greeting</h1> 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
