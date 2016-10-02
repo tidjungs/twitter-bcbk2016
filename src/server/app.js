a717674b26e864bfebedfa7b8f60e7a059398843
komcal <kom.sujautra@gmail.com>
2016-10-02 19:22:15 +0700

    add static file

--- src/server/app.js
+++ src/server/app.js
@@ -4,5 +4,9 @@ const app = express();
 const  twitter  = require('./twitter');
 
 twitter();
+app.use(express.static('build'));
 
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+});
 export default app;
