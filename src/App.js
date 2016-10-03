664c8eb1d215cda95cf3c34145ac62705a4ef083
tidjungs <waewprach.s@ku.th>
2016-10-03 17:14:39 +0700

    add twiter data to client

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.scss';
 import Tweet from './containers/Tweet';
+
+import io from 'socket.io-client';
+var socketURL = 'http://localhost:5000';
+var options ={
+  transports: ['polling'],
+  'force new connection': true
+};
+
 class App extends Component {
   state = {
     tweets: []
   }
   render() {
+    console.log('state' + JSON.stringify(this.state.tweets));
     return (
       <div className={styles.App}>
         <div className={styles.header}>
@@ -20,6 +29,24 @@ class App extends Component {
         <Tweet tweets={this.state.tweets} />
       </div>
     );
+  }
+  componentDidMount() {
+    var self = this;
+    var client1 = io.connect(socketURL, options);
+      client1.on('connect', function(data){
+      client1.on('new tweet', function(data) {
+        self.setState({
+          tweets: [...self.state.tweets, 
+            {
+              name: data.user.name,
+              screen_name: data.user.screen_name,
+              text: data.text,
+              time: data.user.created_at
+            }
+          ]
+        })
+      });
+    });
   }
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export const Card = (tweet, key) => (
   <div key={key} className='card'>
+    <p>{tweet.name}</p>
+    <p>{tweet.screen_name}</p>
+    <p>{tweet.time}</p>
     <p>{tweet.text}</p>
   </div>
 );
