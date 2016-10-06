085b7914494428a22c58f681ce49d546d5f310a4
komcal <kom.sujautra@gmail.com>
2016-10-06 13:32:35 +0700

    removed warning prop key

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../App.scss'
-export const Card = ({tweet, key}) => (
-  <div key={key} className={styles.card}>
+export const Card = ({tweet}) => (
+  <div className={styles.card}>
   	<img src={tweet.profile_image} className={styles.profile_image} role='presentation' />
   	<div className={styles.header}>
     	<p className={styles.name}>{tweet.name}</p>
